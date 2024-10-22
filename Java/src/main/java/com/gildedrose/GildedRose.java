54fcaa5443317481ca490e9e88afed06dc46d96f
Steven TABRI <steven.tabri@open-groupe.com>
2024-10-23 00:30:54 +0200

    a

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,8 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+import java.util.List;
+
 class GildedRose {
     Item[] items;
 
@@ -7,7 +10,12 @@ class GildedRose {
         this.items = items;
     }
 
+
     public void updateQuality() {
+        updateQualityNew();
+    }
+
+    public void updateQualityOld() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -59,4 +67,48 @@ class GildedRose {
             }
         }
     }
+
+
+
+
+
+    protected static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    protected static final String AGED = "Aged Brie";
+    protected static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
+
+    public void updateQualityNew() {
+
+        Arrays.stream(items)
+            .filter(item -> !List.of(BACKSTAGE, AGED, SULFURAS).contains(item.name))
+            .filter(item -> item.quality > 0)
+            .forEach(item -> item.quality--);
+
+        Arrays.stream(items)
+            .filter(item -> item.quality < 50)
+            .peek(item -> item.quality++)
+            .filter(item -> BACKSTAGE.equals(item.name))
+            .filter(item -> item.sellIn < 11)
+            .filter(item -> item.quality < 50)
+            .forEach(item -> item.quality++);
+
+        Arrays.stream(items)
+            .filter(item -> !SULFURAS.equals(item.name))
+            .forEach(item -> item.sellIn--);
+
+        Arrays.stream(items)
+            .filter(item -> item.sellIn < 0)
+            .forEach(this::alterQuality);
+    }
+
+    protected void alterQuality(Item item) {
+        if (AGED.equals(item.name) && item.quality < 50) {
+            item.quality++;
+        } else if (BACKSTAGE.equals(item.name)) {
+            item.quality = 0;
+        } else if (!List.of(SULFURAS, AGED).contains(item.name) && item.quality > 0) {
+            item.quality--;
+        }
+    }
+
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void test30Days() {
+        Item[] items = new Item[] {
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Aged Brie", 2, 0), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            // this conjured item does not work properly yet
+            new Item("Conjured Mana Cake", 3, 6) };
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        for (int i = 0; i < 30; i++) {
+            app.updateQuality();
+        }
+
+
+        // +5 Dexterity Vest, -20, 0
+        assertEquals(-20, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+        //    Aged Brie, -28, 50
+        assertEquals(-28, app.items[1].sellIn);
+        assertEquals(50, app.items[1].quality);
+        //    Elixir of the Mongoose, -25, 0
+        assertEquals(-25, app.items[2].sellIn);
+        assertEquals(0, app.items[2].quality);
+        //    Sulfuras, Hand of Ragnaros, 0, 80
+        assertEquals(0, app.items[3].sellIn);
+        assertEquals(80, app.items[3].quality);
+        //    Sulfuras, Hand of Ragnaros, -1, 80
+        assertEquals(-1, app.items[4].sellIn);
+        assertEquals(80, app.items[4].quality);
+        //    Backstage passes to a TAFKAL80ETC concert, -15, 0
+        assertEquals(-15, app.items[5].sellIn);
+        assertEquals(0, app.items[5].quality);
+        //    Backstage passes to a TAFKAL80ETC concert, -20, 0
+        assertEquals(-20, app.items[6].sellIn);
+        assertEquals(0, app.items[6].quality);
+        //    Backstage passes to a TAFKAL80ETC concert, -25, 0
+        assertEquals(-25, app.items[7].sellIn);
+        assertEquals(0, app.items[7].quality);
+        //    Conjured Mana Cake, -27, 0
+        assertEquals(-27, app.items[8].sellIn);
+        assertEquals(0, app.items[8].quality);
     }
 
 }
